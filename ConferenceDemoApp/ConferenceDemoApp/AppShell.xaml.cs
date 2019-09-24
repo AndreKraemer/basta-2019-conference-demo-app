a74d2b57e7c6663d7f9bad0e987a5c55d8023d13
Andre Kraemer <andre@andrekraemer.de>
2019-09-24 12:16:49 +0200

    MenuItem hinzugefügt

--- ConferenceDemoApp/ConferenceDemoApp/AppShell.xaml.cs
+++ ConferenceDemoApp/ConferenceDemoApp/AppShell.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,20 @@ namespace ConferenceDemoApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppShell : Shell
     {
+        private bool _isLoggedIn;
+        public ICommand LoginCommand { get; }
+
         public AppShell()
         {
             InitializeComponent();
+            LoginCommand = new Command(Login);
+            BindingContext = this;
+        }
+
+        private void Login()
+        {
+            _isLoggedIn = !_isLoggedIn;
+            DisplayAlert("Info", _isLoggedIn ? "Eingelogged" : "Ausgelogged", "Ok");
         }
     }
 }
