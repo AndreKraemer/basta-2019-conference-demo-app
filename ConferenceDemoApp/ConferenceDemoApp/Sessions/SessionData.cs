de69ff65c3e7f792d35e22cd2749e5b3026cab91
Andre Kraemer <andre@andrekraemer.de>
2019-09-24 12:35:36 +0200

    CollectionView hinzugefügt

---
+++ ConferenceDemoApp/ConferenceDemoApp/Sessions/SessionData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConferenceDemoApp.Sessions
+{
+    public static class SessionData
+    {
+        public static IList<Session> Sessions { get; }
+        public static IList<Session> Favorites
+        {
+            get { return Sessions.Where(x => x.IsFavorite).ToList(); }
+        }
+
+        static SessionData()
+        {
+            Sessions = new List<Session>
+            {
+                new Session
+                {
+                    Id = 1,
+                    Title = "EF Core und Xamarin",
+                    SpeakerId = 1,
+                    Speaker = "Wilhelm Brause",
+                    Description = "In diesem Vortrag lernen Sie, wie Sie das EF Core unter Xamarin nutzen.",
+                    IsFavorite = true
+                },
+                new Session
+                {
+                    Id = 2,
+                    Title = "Durchgänngige UIs mit Xamarin.Visuals",
+                    SpeakerId = 2,
+                    Speaker = "Hans Dampf",
+                    Description = "Ein Einblick in Xamarin.Visuals",
+                    IsFavorite = false
+                },
+                new Session
+                {
+                    Id = 3,
+                    Title = "Sichere Datenhaltung in Xamarin Apps",
+                    SpeakerId = 3,
+                    Speaker = "Anna Gramm",
+                    Description =
+                        "Anna Gramm erklärt in diesem Vortrag, wie Sie Ihre Daten vor unberechtigten Zugriffen schützen.",
+                    IsFavorite = true
+                }
+            };
+
+
+
+        }
+    }
+}
---
+++ ConferenceDemoApp/ConferenceDemoApp/Speakers/SpeakerData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace ConferenceDemoApp.Speakers
+{
+    public static class SpeakerData
+    {
+        public static IList<Speaker> Speakers { get; private set; }
+
+        static SpeakerData()
+        {
+            Speakers = new List<Speaker>();
+            Speakers.Add(new Speaker
+            {
+                Id = 1,
+                Name = "Wilhelm Brause",
+                Company = "Software-Brause GmbH",
+                Bio = "Wilhelm Brause ist Senior Developer bei der Software Brause GmbH."
+            });
+
+            Speakers.Add(new Speaker
+            {
+                Id = 2,
+                Name = "Hans Dampf",
+                Company = "SteamBytes Gbr",
+                Bio = "Hans Dampf ist Junior Developer bei der SteamBytes Gbr."
+            });
+
+
+            Speakers.Add(new Speaker
+            {
+                Id = 3,
+                Name = "Anna Gramm",
+                Company = "CryptoBytes AG",
+                Bio = "Anna Gramm ist Security-Spezialist  bei der CryptoBytes AG."
+            });
+        }
+    }
+}
