2bc3b18af508cd0eeba054c4a8d09194d97965c7
Andre Kraemer <andre@andrekraemer.de>
2019-09-24 14:31:15 +0200

    Suche
    Einfache Suche über ein Suchfeld im Header der Übersichtsseiten

---
+++ ConferenceDemoApp/ConferenceDemoApp/Sessions/SpeakersSearchHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ConferenceDemoApp.Sessions
+{
+    public class SessionsSearchHandler: SearchHandler
+    {
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+            if (string.IsNullOrEmpty(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                ItemsSource = SessionData.Sessions.Where(
+                        x => x.Title.ToLower().Contains(newValue.ToLower())
+                        || x.Description.ToLower().Contains(newValue.ToLower())
+                        )
+                    .ToList();
+            }
+        }
+
+        protected override async void OnItemSelected(object item)
+        {
+            base.OnItemSelected(item);
+            var id = (item as Session)?.Id;
+            if (id.HasValue)
+            {
+                await Shell.Current.GoToAsync($"sessions/details?id={id.Value}");
+            }
+        }
+    }
+}
---
+++ ConferenceDemoApp/ConferenceDemoApp/Speakers/SpeakersSearchHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ConferenceDemoApp.Speakers
+{
+    public class SpeakersSearchHandler: SearchHandler
+    {
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+            if (string.IsNullOrEmpty(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                ItemsSource = SpeakerData.Speakers.Where(x => x.Name.ToLower().Contains(newValue.ToLower()))
+                    .ToList<Speaker>();
+            }
+        }
+
+        protected override async void OnItemSelected(object item)
+        {
+            base.OnItemSelected(item);
+            var id = (item as Speaker)?.Id;
+            if (id.HasValue)
+            {
+                await Shell.Current.GoToAsync($"speakers/details?id={id.Value}");
+            }
+        }
+    }
+}
