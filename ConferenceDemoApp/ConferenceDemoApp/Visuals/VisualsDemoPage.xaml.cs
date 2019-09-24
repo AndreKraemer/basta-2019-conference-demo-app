07b7f0d2290ca3481718b1aa3824f07a9550131f
Andre Kraemer <andre@andrekraemer.de>
2019-09-24 15:04:07 +0200

    Visuals Basisseite angelegt

---
+++ ConferenceDemoApp/ConferenceDemoApp/Visuals/VisualsDemoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConferenceDemoApp.Visuals
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VisualsDemoPage : ContentPage
+    {
+        public VisualsDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
